9f97261dbe307ca4ea6b178b4fc11d8de9c7b890
johnc <johncunan07@gmail.com>
2024-09-21 07:37:24 +0800

    update

--- public/index.php
+++ public/index.php
@@ -5,14 +5,28 @@ declare(strict_types=1);
 use Slim\Factory\AppFactory;
 use Slim\Psr7\Request;
 use Slim\Psr7\Response;
+use DI\ContainerBuilder;
 
+define('APP_ROOT', dirname(__DIR__));
+    
 require __DIR__ . '/../vendor/autoload.php';
+
+//Dependency Injection
+$builder = new ContainerBuilder;
+$container = $builder->addDefinitions(APP_ROOT . '/config/definitions.php')
+                    ->build();
+AppFactory::setContainer($container);
 
 $app = AppFactory::create();
 
 //Routes
 $app->get('/api', function (Request $request, Response $response) {
-    $response->getBody()->write("Hello world");
+    
+    $repository = $this->get(App\Repositories\ProductRepository::class);
+    $data = $repository->getAll();
+    $body = json_encode($data);
+    $response->getBody()->write($body);
+
     return $response;
 });
 
