90ea0925c212eec994ec90a65b1b9437358b9af4
johnc <johncunan07@gmail.com>
2024-09-21 10:26:59 +0800

    added product repositories

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@ use Slim\Factory\AppFactory;
 use Slim\Psr7\Request;
 use Slim\Psr7\Response;
 use DI\ContainerBuilder;
+use Slim\Handlers\Strategies\RequestResponseArgs;
 
 define('APP_ROOT', dirname(__DIR__));
     
@@ -17,17 +18,39 @@ $container = $builder->addDefinitions(APP_ROOT . '/config/definitions.php')
                     ->build();
 AppFactory::setContainer($container);
 
+//create slim app
 $app = AppFactory::create();
+
+//Route collector
+$collector = $app->getRouteCollector();
+$collector->setDefaultInvocationStrategy(new RequestResponseArgs);
 
 //Routes
 $app->get('/api', function (Request $request, Response $response) {
     
+    //Create ProductRepository.php class object with DI
     $repository = $this->get(App\Repositories\ProductRepository::class);
+    //Use the method in that class
     $data = $repository->getAll();
+    //Convert the extracted data to JSON
     $body = json_encode($data);
+    //Send the response
     $response->getBody()->write($body);
 
-    return $response;
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->get('/api/users/{id:[0-9]+}', function (Request $request, Response $response, string $id) {
+
+    $repository = $this->get(App\Repositories\ProductRepository::class);
+    
+    $data = $repository->getById((int) $id);
+    
+    $body = json_encode($data);
+    
+    $response->getBody()->write($body);
+    
+    return $response->withHeader('Content-Type', 'application/json');
 });
 
 $app->run();
